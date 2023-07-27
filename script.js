4d912159f1751a6eab26b364ee7faf6212e03131
VkusnoePechenko <zerotul777@gmail.com>
2023-07-27 17:46:15 +0300

    1.1

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
-document.addEventListener("keydown", function(event) {
-    jump();
-});
-
  function jump () {
     if (dino.classList != "jump") {
-        dino.classList.add("jump")
+        dino.classList.add("jump");
+    
+        setTimeout(function() {
+            dino.classList.remove("jump");
+        }, 300);
     }
-    setTimeout( function() {
-        dino.classList.remove("jump")
-    }, 300)
  }
 
-//  let isAlive = setInterval ( function() {
-//     let dinoTop  = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-//     let cactusLeft  = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+ let isAlive = setInterval (function() {
+        // get current dino Y position
+    let dinoTop  = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+        // get currrent cactus X position
+    let cactusLeft  = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-//     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >=140) {
-//         alert("GAME OVER!")
-//     }
-//  }, 10)
+        // detect position
+    if (cactusLeft < 45 && cactusLeft > 0 && dinoTop >=140) {
+        alert("Game Over!");
+    }
+ }, 10);
+
+ document.addEventListener("keydown", function(event) {
+    jump();
+});
